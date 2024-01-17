d7a559e2b5fe57b5aea4e3b4c4ce1ebfe54dcb54
Paulo <paulo.ipa9@gmail.com>
2024-01-17 20:39:25 -0300

    Jogo número secreto

---
+++ jogoNumeroSecreto.js
@@ -0,0 +1,19 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 2;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert (`O número secreto é maior que ${chute}`);
+        }
+        tentativas = tentativas + 1;
+    }
+}
